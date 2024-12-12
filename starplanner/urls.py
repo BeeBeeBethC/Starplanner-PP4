1f6032073850390b66339110f4080074dd2cf323
Bethan Cottell <cottellbc@gmail.com>
2024-12-12 14:35:41 +0000

    debugging continues

--- starplanner/urls.py
+++ starplanner/urls.py
@@ -1,9 +1,7 @@
 from . import views
 from django.urls import path
-from django.views.generic import TemplateView
-from starplanner.views import BaseView
+from .views import PostList
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('home/', BaseView.as_view(template_name="index.html")),
+    path('', views.PostList.as_view(), name='home'),
 ]
--- starplanner/views.py
+++ starplanner/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-# from .models import Post
+from django.views import generic
+from .models import Post
 
 
 # Create your views here.
-def home(request):
-    return render(request,'base.html')
-
-class BaseView(TemplateView):
-    template_name = "index.html"
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1)
+    template_name = "starplanner/index.html"
+    paginate_by = 6
