91a5ab074fdfbfc394eb12785b710ea0d7da31d6
Bethan Cottell <cottellbc@gmail.com>
2025-01-13 14:37:49 +0000

    models.py updated, migrations updated

--- starplanner/models.py
+++ starplanner/models.py
@@ -2,13 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Task(models.Model):
-    name = models.CharField(max_length=100)
-    task_id = models.AutoField(primary_key=True)
-    user_id = models.CharField(max_length=100, unique=True)
+    task = models.AutoField(primary_key=True)
+    user = models.CharField(max_length=100, unique=True)
     priority = models.CharField(max_length=100)
-    description = models.CharField(max_length=100)
-    comment = models.TextField(max_length=500)
-    created_on = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(max_length=500)
     
+# this is a full string representation of the tasks
     def __str__(self):
-        return self.name
+        return (f"ID:{self.task}: Added By: {self.user}, Priority: {self.priority}, Task Description: {self.description}")
