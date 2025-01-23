f3dea1eb3163d7d8ec9ee4f092d9a99e20195712
Bethan Cottell <cottellbc@gmail.com>
2025-01-23 09:45:40 +0000

    moved registration form from starplanner to accounts

---
+++ accounts/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+from django.contrib.auth.models import User
+
+class RegisterForm(forms.Form):
+    register_user = forms.CharField(label="Your Name", max_length=50)
--- starplanner/urls.py
+++ starplanner/urls.py
@@ -1,9 +1,10 @@
 from . import views
+# from accounts.views import accounts
 from django.urls import path
 
 urlpatterns = [
     path('', views.starplanner, name='starplanner'),
     path('planner/', views.planner_home, name='planner_home'),
-    path('accounts/', views.accounts, name='accounts'),
+   # path('accounts/', accounts.views.accounts, name='accounts'),
     path('safe/', views.SafeView.as_view(), name='protected_logged_in')
 ]
