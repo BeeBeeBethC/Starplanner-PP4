594f9f7473e82ba4e6442563a41f702d2c1c9fd2
Beth Cottell <cottellbc@gmail.com>
2025-01-30 10:54:34 +0000

    task form added for planner

--- starplanner/forms.py
+++ starplanner/forms.py
@@ -1,1 +1,19 @@
 from django import forms
+from .models import Task
+
+class TaskForm(forms.ModelForm):
+    class Meta:
+        model = Task
+        fields= '__all__'
+        labels = {
+            'task': 'Task',
+            'user': 'User',
+            'priority': 'Priority',
+            'description': 'Description',
+        }
+        widgets = {
+            'task':forms.NumberInput(attrs={'placeholder':'e.g 1', 'class':'form-control'}),
+            'user':forms.TextInput(attrs={'placeholder':'e.g Jbloggs', 'class':'form-control'}),
+            'priority':forms.RadioSelect(attrs={'placeholder':'select priority', 'class':'form-control'}),
+            'description':forms.Textarea(attrs={'placeholder':'describe your task here', 'class':'form-control'}),
+        }
