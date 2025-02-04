be3bd8eb69bbd806540bf04656b7438cf07f66da
Beth Cottell <cottellbc@gmail.com>
2025-02-04 11:02:02 +0000

    added navigation links adjusted views

--- starplanner/views.py
+++ starplanner/views.py
@@ -16,7 +16,7 @@ def starplanner(request):
 
 
 # create_task_view
-@login_required(login_url='login')
+@login_required
 def create_task(request):
     form = TaskForm()
     if request.method == 'POST':
@@ -29,16 +29,16 @@ def create_task(request):
 
 
 # read_task_view
-@login_required(login_url='login')
+@login_required
 def read_task(request):
     tasks = Task.objects.all()
     return render(request, 'read_task.html', {'tasks': tasks})
 
 
 # update_task_view
-@login_required(login_url='login')
+@login_required
 def update_task(request, task_id):
-    task = get_object_or_404(Task, pk=task_id)
+    task = get_object_or_404(Task, task=task_id)
     form = TaskForm(request.POST, instance=task)
     if request.method == "POST":
         form = TaskForm(request.POST, instance=task)
@@ -51,10 +51,10 @@ def update_task(request, task_id):
 
 
 # delete_task_view
-@login_required(login_url='login')
+@login_required
 def delete_task(request, task_id):
-    tasks = get_object_or_404(Task, pk=task_id)
-    if request.method == 'POST':
-        tasks.delete()
+    task = get_object_or_404(Task, task=task_id)
+    if request.method == "POST":
+        task.delete()
         return redirect('read_task')
-    return render(request, 'delete_task.html', {'tasks': tasks})
+    return render(request, 'delete_task.html', {'task': task})
