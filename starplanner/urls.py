8e23f4c3a3a8d764f5174589bf9fd43fdc63cabe
Beth Cottell <cottellbc@gmail.com>
2025-02-10 12:20:03 +0000

    reconnected the database

--- starplanner/urls.py
+++ starplanner/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.starplanner, name='starplanner'),
-    path('create/', views.create_task, name="create"),
-    path('read/', views.read_task, name="read"),
-    path('update/<int:task_id>/', views.update_task, name="update"),
-    path('delete/<int:task_id>/', views.delete_task, name="delete"),
+   # path('create/', views.create_task, name="create"),
+   # path('read/', views.read_task, name="read"),
+   # path('update/<int:task_id>/', views.update_task, name="update"),
+   # path('delete/<int:task_id>/', views.delete_task, name="delete"),
 ]
--- starplanner/views.py
+++ starplanner/views.py
@@ -5,8 +5,7 @@ from django.template import loader, RequestContext
 from django.views.generic import View
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from .forms import TaskForm
-from .models import Task
+
 
 
 # home - starplanner home view!!
@@ -15,46 +14,3 @@ def starplanner(request):
     return HttpResponse(template.render())
 
 
-# create_task_view
-@login_required
-def create_task(request):
-    form = TaskForm()
-    if request.method == 'POST':
-        form = TaskForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('read/')
-    context = {'form': form}
-    return render(request, 'create_task.html', context)
-
-
-# read_task_view
-@login_required
-def read_task(request):
-    tasks = Task.objects.all()
-    return render(request, 'read_task.html', {'tasks': tasks})
-
-
-# update_task_view
-@login_required
-def update_task(request, task_id):
-    task = get_object_or_404(Task, task=task_id)
-    form = TaskForm(request.POST, instance=task)
-    if request.method == "POST":
-        form = TaskForm(request.POST, instance=task)
-        if form.is_valid():
-            form.save()
-            return redirect('read_task')
-        else:
-            form=TaskForm(instance=task)
-    return render(request, 'update_task.html', {'form': form, 'task': task})
-
-
-# delete_task_view
-@login_required
-def delete_task(request, task_id):
-    task = get_object_or_404(Task, task=task_id)
-    if request.method == "POST":
-        task.delete()
-        return redirect('read_task')
-    return render(request, 'delete_task.html', {'task': task})
