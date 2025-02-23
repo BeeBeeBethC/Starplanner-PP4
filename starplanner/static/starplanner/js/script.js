249c02ef18817337f7fb1a5994e7a7250972cd55
Beth Cottell <cottellbc@gmail.com>
2025-02-23 11:40:54 +0000

    js modal now showing and dismissing debug continues

--- starplanner/static/starplanner/js/script.js
+++ starplanner/static/starplanner/js/script.js
@@ -1,2 +1,17 @@
 console.log('Custom JavaScript is loaded and working!');
 
+function showPermissionModal() {
+    document.getElementById('permissionModal').style.display = 'block';
+}
+
+function closePermissionModal() {
+    document.getElementById('permissionModal').style.display = 'none';
+}
+
+// Close modal when clicking outside
+window.onclick = function(event) {
+    const modal = document.getElementById('permissionModal');
+    if (event.target == modal) {
+        modal.style.display = 'none';
+    }
+};
